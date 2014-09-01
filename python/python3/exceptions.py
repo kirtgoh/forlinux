7a093c96f0a43554e132e5db84cfa3da921988ac
kirtgoh <kirtgoh@gmail.com>
2014-09-01 22:01:08 +0800

    py3-4: exceptions

---
+++ python/python3/exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+try:
+	fh = open('xlines.txt')
+	for line in fh.readlines():
+		print(line)	
+
+except IOError as e:
+	print("something bad happened ({})".format(e))
+
+print("after badness")
